4ecbabb8eb448683041bf28f718d942778e0961b
ranjanghate1 <rghate@gammastack.com>
2021-07-27 19:15:54 +0530

    add data to firebase

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
 
 function renderCafe(doc){
   let li = document.createElement('li');
@@ -17,4 +19,14 @@ db.collection('cafes').get().then((snapshot) => {
   snapshot.docs.forEach( doc => {
     renderCafe(doc);
   });
+});
+
+form.addEventListener( 'submit', (e) => {
+  e.preventDefault();
+  db.collection('cafes').add({
+    name: form.name.value,
+    city: form.city.value
+  });
+  form.name.value = '';
+  form.city.value = '';
 });
